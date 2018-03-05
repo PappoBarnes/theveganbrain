3cced420abd747c3b690fa3834f437676b1b7adf
PappoBarnes <33037717+PappoBarnes@users.noreply.github.com>
2018-03-05 11:44:33 +0000

    Add files via upload

---
+++ The-Vegan-Brain-05-Mar/js/dropdown.js
@@ -0,0 +1,25 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) { // IF STATEMENT - 
+
+    var dropdowns = document.getElementsByClassName("dropdown-content"); 
+                  // declares varaible name for element to be customised.
+    var i;        // dellares variable for i as a counter.
+
+    for (i = 0; i < dropdowns.length; i++) { 
+                  //for loop- checks the length of the dropdowns variable
+      var openDropdown = dropdowns[i]; 
+                 
+                  // and passes it to the variable openDropdown
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
