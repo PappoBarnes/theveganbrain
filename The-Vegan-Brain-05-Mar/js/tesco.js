d1b04c0c277672529659b03b9e63ae194c801eea
PappoBarnes <33037717+PappoBarnes@users.noreply.github.com>
2018-03-07 19:58:45 +0000

    Add files via upload

---
+++ The-Vegan-Brain-05-Mar/js/tesco.js
@@ -0,0 +1,40 @@
+function getSearch(){
+const searchTerm = document.getElementById("search").value;
+const offset = 0;
+const limit = 10;
+
+$(function() {
+        
+        $.ajax({
+            url:`https://dev.tescolabs.com/grocery/products/?query=${searchTerm}&offset=${offset}&limit=${limit}`,
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","d4a7b004a07b49e4b341bfe452cf13c7"); //We added the key supplied from Tesco.
+            },
+            type: "GET",
+            // Request body
+            data: "{body}",  //Not sure if we need to add code here to display on the page
+        })
+        .done(function(data) {
+            alert("success");
+            // document.write(resultSpace.innerHTML=(data));
+            console.log(data); //view in the console, click on the triangles to see the data structure
+            const products = data.uk.ghs.products.results; //this gets the actual list of products and assigns to an array
+            products.forEach(function(product){
+                console.log(product.name);
+            })
+        })
+        .fail(function() {
+            alert("error");
+        });
+    });
+
+        console.log(searchTerm);
+        console.log(`You entered ${searchTerm}`)
+ };       
+const searchBtn=document.getElementById("searchButton");
+searchBtn.addEventListener("click",function(){
+    getSearch()});
+    
+        
+//   
